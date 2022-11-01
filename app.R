a6a3bc837297020fc87c817fc179db69758c3dac
Zach Fogel <zachfogel@Zachs-Air.hitronhub.home>
2022-11-01 10:21:58 -0700

    Made it faster to adjust transparency.
    1) changed from a leaflet object to a Tmap object
    2) hardcoded 1 in instead of input$alpha inside renderTmap (which replaced renderLeaflet) when setting the initial alpha values
    3) added a zindex value for the different tmap elements, which is essentially the order that they are on the page
    4) outside of renderTmap(), I made a tmapProxy object which allowed me to remove the tm_shape objects for the grid cells (which all had the same zindex) and draw them back in with a lower alpha.
    5) I tried doing the same thing with the linear and areal features but that took a lot longer than just the grid cells, so I commented that part out but left it in in case you want to try it.

--- app.R
+++ app.R
@@ -1,3 +1,5 @@
+## Load Required Libraries
+library(shiny)
 library(sf)
 library(DT)
 library(tmap)
@@ -5,176 +7,239 @@ library(dplyr)
 library(leaflet)
 library(tidyverse)
 library(shinydashboard)
+library(shinybusy)
 
-tmap_mode("view")
-tmap_options(check.and.fix = TRUE)
-tmap_options(basemaps = c(Esri.WorldImagery="Esri.WorldImagery", Esri.NatGeoWorldMap="Esri.NatGeoWorldMap"))
-grid <- st_read("data/wolverines.gpkg", "grid5k", quiet=T)
+## Set up map options
+tmap_mode('view')
+tmap_options(check.and.fix = T)
+# Basemap 
+tm_basemap(leaflet::providers$Esri.WorldImagery)
+
+## Load Data
+grid <- st_read('data/wolverines.gpkg', 'grid5k', quiet = T)
 linear <- st_read("data/wolverines.gpkg", "linear_features", quiet=T)
 areal <- st_read("data/wolverines.gpkg", "areal_features", quiet=T)
 factors <- st_read("data/wolverines.gpkg", "survey_factors", quiet=T)
 
 ui = dashboardPage(
-  dashboardHeader(title = "Wolverines Survey"),
+  dashboardHeader(title = 'Wolverines Survey'),
   dashboardSidebar(
+    
     sidebarMenu(
-        menuItem("Survey factors", tabName = "fri", icon = icon("th"))
-    ),
-    selectInput("inv", label = "Feature type:", choices = names(factors)[3:22], selected='merge100_pct'),
-    selectInput("style", label="Select style:", choices=c("jenks","kmeans","quantile","pretty","equal"), selected="quantile"),
-    sliderInput("alpha", label="Transparency:", min=0, max=1, value=1, step=0.1, ticks=FALSE),
+      menuItem('Survey Factors', tabName = 'fri', icon = icon('th'))
+    ), # sidebarMenu
+    
+    # Select feature type (merge_100, percent_forest, etc) to view
+    selectInput("inv", label = "Feature type:", choices = names(factors)[3:22], 
+                selected='merge100_pct'),
+    # Style for binning cells for display
+    selectInput("style", label="Select style:", 
+                choices=c("jenks","kmeans","quantile","pretty","equal"), 
+                selected="quantile"),
+    # Set transparency
+    sliderInput("alpha", label="Transparency:", min=0, max=1, value=1, step=0.1, 
+                ticks=FALSE),
     hr(),
-    selectInput("factors", label = "Select features:", multiple=TRUE, choices = names(factors)[3:22], selected=c('merge100_pct','elev_median','elev_sd','forest_pct','water_pct')),
-    sliderInput("clusters", label="Number of clusters:", min=0, max=20, value=4, ticks=FALSE),
+    # features to cluster by
+    selectInput("factors", label = "Select features:", multiple=TRUE, 
+                choices = names(factors)[3:22], 
+                selected=c('merge100_pct','elev_median','elev_sd','forest_pct',
+                           'water_pct')),
+    # how many clusters
+    sliderInput("clusters", label="Number of clusters:", min=0, max=20, value=4, 
+                ticks=FALSE),
+    # button to generate clusters
     actionButton("clustButton", "Generate clusters"),
     hr(),
-    sliderInput("size", label="Sample size per strata:", min=0, max=100, value=25, step=5, ticks=FALSE),
+    
+    # select random sites
+    # slider for how many cells to select from each bin
+    sliderInput("size", label="Sample size per strata:", min=0, max=100, 
+                value=25, step=5, ticks=FALSE),
+    # button to select random sites
     actionButton("goButton", "Select random grids")
-  ),
+
+  ), # dashboardSidebar
+  
   dashboardBody(
     tabItems(
-        tabItem(tabName="fri",
-            fluidRow(
+      tabItem(tabName = 'fri',
+              fluidRow(
                 tabBox(
-                    id = "one", width="12",
-                    tabPanel("Mapview", leafletOutput("map1", height=900)),
-                    tabPanel("Clusters", DT::dataTableOutput("tab1")),#pre(includeText("bc10.txt")))
-                    tabPanel("Similarity", DT::dataTableOutput("tab2"))#pre(includeText("bc10.txt")))
-                )
-            )
-        )
-    )
-  )
-)
+                  id = 'one', width = '12',
+                  tabPanel('Mapview', tmapOutput('map1', height = 900)),
+                  tabPanel('Clusters', DT::dataTableOutput('tab1')),
+                  tabPanel('Similarity', DT::dataTableOutput('tab2'))
+                ) # tabBox
+              ) # fluidRow
+      ) # tabItem
+    ) # tabItems
+  ) # dashboardBody
+) # ui
 
-server = function(input, output) {
-
-    n1 <- eventReactive(input$goButton, {
-        n = sample_n(clusters(), input$size*as.numeric(input$clusters))
-        #nn <- tibble(type="simple", id=pull(n, id))
-        #readr::write_csv(nn, paste0('samples/simple_random_c',input$clusters,'_n',input$size,'.csv'))
-        #n
-    })
-
-    n2 <- eventReactive(input$goButton, {
-        n <- group_by(clusters(), clusters) %>% sample_n(size=input$size)
-        #nn <- tibble(type="stratified", id=pull(n, id))
-        #readr::write_csv(nn, paste0('samples/stratified_random_c',input$clusters,'_n',input$size,'.csv'))
-        #n
-    })
-
-    clusters <- eventReactive(input$clustButton, {
-        x <- mutate(factors,
-            placer_pct=ifelse(is.na(placer_pct),0,placer_pct),
-            quartz_pct=ifelse(is.na(quartz_pct),0,quartz_pct),
-            recent_fires_pct=ifelse(is.na(recent_fires_pct),0,recent_fires_pct),
-            area500_pct=ifelse(is.na(area500_pct),0,area500_pct),
-            line500_pct=ifelse(is.na(line500_pct),0,line500_pct),
-            merge100_pct=ifelse(is.na(merge100_pct),0,merge100_pct),
-            water_pct=ifelse(is.na(water_pct),0,water_pct),
-            forest_pct=ifelse(is.na(forest_pct),0,forest_pct),
-            wetland_pct=ifelse(is.na(wetland_pct),0,wetland_pct))
-        #clust <- kmeans(scale(cbind(x$merge100_pct,x$elev_median,x$elev_sd,x$forest_pct)),input$clusters)$cluster
-        y = select(x, unlist(input$factors)) %>% st_drop_geometry()
-        clust <- kmeans(scale(y),input$clusters)$cluster
-        x <- mutate(x, clusters=clust)
-    })
-
-    output$map1 <- renderLeaflet({
-        if (input$inv=='forest_pct') {
-            pal='YlGn'
-        } else if (input$inv %in% c('water_pct','wetland_pct')) {
-            pal='PuBu'
-        } else if (input$inv %in% c('quartz_pct','placer_pct')) {
-            pal='Greys'
-        } else if (input$inv %in% c('elev_median','elev_min','elev_max','elev_sd')) {
-            pal='-BrBG'
-        } else {
-            pal='Reds'
-        }
-        if (input$clustButton) {
-            m <- tm_shape(clusters()) + tm_fill('clusters', palette='Set1', alpha=input$alpha, group="Clusters")
-        } else {
-            m <- tm_shape(factors) + tm_fill(input$inv, palette=pal, style=input$style, alpha=input$alpha, colorNA=NULL, group=input$inv, title=input$inv)
-        }
-        m <- m + tm_shape(grid) + tm_borders() +
-            tm_shape(linear) + tm_lines(col='black', alpha=input$alpha, group="Linear features") +
-            tm_shape(areal) + tm_fill(col='red', alpha=input$alpha, group="Areal features")
-        if (input$goButton) {
-            m <- m + tm_shape(n1()) + tm_borders(col='green', lwd=2, group="Simple random") +
-                tm_shape(n2()) + tm_borders(col='yellow', lwd=2, group="Stratified random")
-        }
-        m <- m + tm_legend(position = c("right", "top"), frame=TRUE)
-        lf <- tmap_leaflet(m)
-        lf %>% leaflet::hideGroup(c("Areal disturbances","Linear disturbances","Areal features","Linear features","Elevation","Simple random"))
-    })
-
-    dta1 <- reactive({
-        x <- clusters() %>% st_drop_geometry() %>%
-            #select(merge100_pct,elev_median,elev_sd,forest_pct,clusters) %>%
-            group_by(clusters) %>%
-            summarize(n=n(),
-                merge100_pct=round(mean(merge100_pct),1),
-                recent_fires_pct=round(mean(recent_fires_pct),1),
-                #quartz_pct=round(mean(quartz_pct),1),
-                #placer_pct=round(mean(placer_pct),1),
-                elev_median=round(mean(elev_median),1),
-                elev_sd=round(mean(elev_sd),1),
-                forest_pct=round(mean(forest_pct),1),
-                water_pct=round(mean(water_pct),1),
-                wetland_pct=round(mean(wetland_pct),1),
-                )
-        #x2 = as_tibble(x1) %>% slice(1) %>% unlist(., use.names=FALSE)
-        #x = bind_cols(Attribute=names(x1), Value=x2)
-    })
-
-	output$tab1 <- DT::renderDataTable({
-        x = dta1() #%>% filter(!Attribute %in% casVars)
-		datatable(x, rownames=F, options=list(dom = 'tip', scrollX = TRUE, scrollY = TRUE, pageLength = 25), class="compact")
-    })
-
-    #dta2 <- reactive({
-    #})
-
-    output$tab2 <- DT::renderDataTable({
-        simple <- tibble(type="simple", id=pull(n1(), id)) %>%
-            left_join(factors)
-
-        stratified <- tibble(type="stratified", id=pull(n2(), id)) %>%
-            left_join(factors)
-
-        ks_simple <- tibble(type="simple",
-            merge100_pct = ks.test(factors$merge100_pct, simple$merge100_pct)[[1]],
-            #merge500_pct = ks.test(factors$merge500_pct, simple$merge500_pct)[[1]],
-            placer_pct = ks.test(factors$placer_pct, simple$placer_pct)[[1]],
-            quartz_pct = ks.test(factors$quartz_pct, simple$quartz_pct)[[1]],
-            recent_fires_pct = ks.test(factors$recent_fires_pct, simple$recent_fires_pct)[[1]],
-            benchmark_pct = ks.test(factors$benchmark_pct, simple$benchmark_pct)[[1]],
-            elev_median = ks.test(factors$elev_median, simple$elev_median)[[1]],
-            elev_sd = ks.test(factors$elev_sd, simple$elev_sd)[[1]],
-            forest_pct = ks.test(factors$forest_pct, simple$forest_pct)[[1]],
-            wetland_pct = ks.test(factors$wetland_pct, simple$wetland_pct)[[1]],
-            water_pct = ks.test(factors$water_pct, simple$water_pct)[[1]])
-
-        ks_stratified <- tibble(type="stratified",
-            merge100_pct = ks.test(factors$merge100_pct, stratified$merge100_pct)[[1]],
-            #merge500_pct = ks.test(factors$merge500_pct, stratified$merge500_pct)[[1]],
-            placer_pct = ks.test(factors$placer_pct, stratified$placer_pct)[[1]],
-            quartz_pct = ks.test(factors$quartz_pct, stratified$quartz_pct)[[1]],
-            recent_fires_pct = ks.test(factors$recent_fires_pct, stratified$recent_fires_pct)[[1]],
-            benchmark_pct = ks.test(factors$benchmark_pct, stratified$benchmark_pct)[[1]],
-            elev_median = ks.test(factors$elev_median, stratified$elev_median)[[1]],
-            elev_sd = ks.test(factors$elev_sd, stratified$elev_sd)[[1]],
-            forest_pct = ks.test(factors$forest_pct, stratified$forest_pct)[[1]],
-            wetland_pct = ks.test(factors$wetland_pct, stratified$wetland_pct)[[1]],
-            water_pct = ks.test(factors$water_pct, stratified$water_pct)[[1]])
-        
-        ks <- rbind(ks_simple, ks_stratified)
-
-        datatable(ks, rownames=F, options=list(dom = 'tip', scrollX = TRUE, scrollY = TRUE, pageLength = 25), class="compact") %>%
-            formatRound(columns=c('merge100_pct','placer_pct','quartz_pct','recent_fires_pct','benchmark_pct','elev_median','elev_sd',
-                'forest_pct','wetland_pct','water_pct'), digits=3)
-    })
-
+server <- function(input, output) {
+  #' Create reactiveValue: table of cells, separated into different bins based on
+  #' kmeans clustering
+  clusters <- eventReactive(input$clustButton, {
+    # replace NAs with 0s
+    x <- factors %>% 
+      mutate(
+             placer_pct=ifelse(is.na(placer_pct),0,placer_pct),
+             quartz_pct=ifelse(is.na(quartz_pct),0,quartz_pct),
+             recent_fires_pct=ifelse(is.na(recent_fires_pct),0,recent_fires_pct),
+             area500_pct=ifelse(is.na(area500_pct),0,area500_pct),
+             line500_pct=ifelse(is.na(line500_pct),0,line500_pct),
+             merge100_pct=ifelse(is.na(merge100_pct),0,merge100_pct),
+             water_pct=ifelse(is.na(water_pct),0,water_pct),
+             forest_pct=ifelse(is.na(forest_pct),0,forest_pct),
+             wetland_pct=ifelse(is.na(wetland_pct),0,wetland_pct)
+            ) # mutate
+    
+    # select only factors that user wants to cluster by
+    # st_drop_geometry() drops geom field from table (geom describes what type of
+    # feature it is and has some numbers describing it)
+    y <- select(x, unlist(input$factors)) %>% st_drop_geometry()
+    
+    # Now actually cluster cells
+    # kmeans() returns a list; $cluster object is a vector where the name is the
+    # cell number and the value is what cluster it's in
+    clust <- kmeans(scale(y), input$clusters)$cluster
+    # add field to x with what cluster the cell is in
+    x <- x %>%
+      mutate(clusters = clust)
+  })
+  
+  # n1 is random sampling; ie same number is not sampled from each cluster 
+  n1 <- eventReactive(input$goButton, {
+    # sample from clusters() (defined above), number to be samples is number of 
+    #   samples per cluster * number of clusters
+    n <- sample_n(clusters, input$size * as.numeric(input$clusters))
+  })
+  
+  # n2 is stratified random sampling; ie same number sampled from each cluster
+  n2 <- eventReactive(input$goButton, {
+    n <- clusters %>%
+      group_by(clusters) %>%
+      sample_n(size = input$size)
+  })
+  
+  #### Map Output ----
+  
+  output$map1 <- renderTmap({
+    # print('in renderTmap')
+    # print(pal)
+    
+    # Set color palette based on selected characteristics
+    if (input$inv == 'forest_pct') {
+      pal <- 'YlGn'
+    } else if (input$inv %in% c('water_pct','wetland_pct')) {
+      pal <- 'PuBu'
+    } else if (input$inv %in% c('quartz_pct','placer_pct')) {
+      pal <- 'Greys'
+    } else if (input$inv %in% c('elev_median','elev_min','elev_max','elev_sd')) {
+      pal <- '-BrBG'
+    } else {
+      pal <- 'Reds'
+    }
+    
+    # If the user clicks the button to generate clusters
+    if (input$clustButton) {
+      m <- tm_shape(clusters) + tm_fill('clusters', palette = 'Set1',
+                                        alpha = 1,
+                                        group = 'Clusters',
+                                        # zindex determines the order that elements
+                                        # appear on the page; an element with a
+                                        # higher z index appears in front of a lower
+                                        # zindex
+                                        zindex = 500)
+    } else {
+      m <- tm_shape(factors) + tm_fill(input$inv, palette = pal, 
+                                       style = input$style,
+                                       alpha = 1,
+                                       colorNA = NULL, 
+                                       group = input$inv,
+                                       title = input$inv,
+                                       zindex = 500)
+    }
+    
+    # Base map
+    m <- m + tm_shape(grid) + tm_borders() +
+      tm_shape(linear) + tm_lines(col = 'black',
+                                  alpha = 1,
+                                  group = 'linfeat', zindex = 560) +
+      tm_shape(areal) + tm_fill(col = 'red',
+                                alpha = 1,
+                                group = 'arealfeat', zindex = 570)
+    
+    print('added linear and areal features')
+    
+    # add randomly selected cells
+    if (input$goButton) {
+      m <- m + tm_shape(n2()) + tm_borders(col = 'green', lwd = 2, 
+                                           group = 'simp.random', zindex = 600) +
+               tm_shape(n2()) + tm_borders(col = 'yellow', lwd = 2, 
+                                           group = 'strat.random', zindex = 600)
+    }
+    
+    m <- m + tm_legend(position = c('right', 'top'), frame = T)
+    # print('reached end of tmap rendering')
+    m
+  }) # renderTmap
+  
+  # ignoreInit means that it isn't triggered when the app opens, only when the 
+  # user moves the slider
+  observeEvent(input$alpha, ignoreInit = T, handlerExpr = {
+    # Couldn't get pal to be within the scope of this function so I just copied
+    # and pasted it 
+    if (input$inv == 'forest_pct') {
+      pal <- 'YlGn'
+    } else if (input$inv %in% c('water_pct','wetland_pct')) {
+      pal <- 'PuBu'
+    } else if (input$inv %in% c('quartz_pct','placer_pct')) {
+      pal <- 'Greys'
+    } else if (input$inv %in% c('elev_median','elev_min','elev_max','elev_sd')) {
+      pal <- '-BrBG'
+    } else {
+      pal <- 'Reds'
+    }
+    
+    if (input$clustButton > 0) {
+      # create new layers for clusters/factors with alpha specified
+      m1 <- tm_shape(clusters) + tm_fill('clusters', palette = 'Set1',
+                                              alpha = input$alpha,
+                                              group = 'Clusters',
+                                              zindex = 500)
+    } else {
+      m1 <- tm_shape(factors) + tm_fill(input$inv, palette = pal, style = input$style,
+                                        alpha = input$alpha,
+                                        colorNA = NULL, 
+                                        group = input$inv,
+                                        title = input$inv,
+                                        zindex = 500)
+      
+      m.linareal <- tm_shape(linear) + tm_lines(col = 'black',
+                                                alpha = input$alpha,
+                                                group = 'linfeat', zindex = 560) +
+                    tm_shape(areal) + tm_fill(col = 'red',
+                                              alpha = input$alpha,
+                                              group = 'arealfeat', zindex = 570)
+    }
+    tmapProxy('map1',
+              x = {tm_remove_layer(500) +
+                   tm_remove_layer(560) +
+                   tm_remove_layer(570) +
+                   m.linareal +
+                   m1 
+                  }
+             ) # tmapProxy 
+  }) # observeEvent
 }
+
 shinyApp(ui, server)
+
+
+
+
+
+
+
